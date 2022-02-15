b796ec6200f91ad3ae1de45ff1760420f7beda11
israaHamdi <israaaburayya2012@gmail.com>
2022-02-15 16:31:49 +0200

    add delete function and eventlistener to form to take name country RELATES#11

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,16 @@
 const getElement = (element) => {
     return document.querySelector(element)
 }
+const  deleteChild =(element)  => {
+    var child = element.lastElementChild; 
+    while (child) {
+        element.removeChild(child);
+        child = element.lastElementChild;
+    }
+}
 
 const handlePicture = (data) => {
-    console.log(data)
+    deleteChild(getElement('.figure'))
     const image=document.createElement('img')
     image.className='img'
     image.alt=data.hits[0].tags
@@ -11,3 +18,13 @@ const handlePicture = (data) => {
     getElement('.figure').appendChild(image)
 }
 fetch ("GET","https://pixabay.com/api/?key=25715508-30ede2cb1753fb52763f43dcd&q=gaza&image_type=photo",handlePicture)
+
+getElement('.form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    console.log(getElement('.input').value)
+    const inputValue=getElement('.input').value
+    const urlPicture=`https://pixabay.com/api/?key=25715508-30ede2cb1753fb52763f43dcd&q=${inputValue}&image_type=photo`
+    fetch("GET",urlPicture,handlePicture)
+
+})
+

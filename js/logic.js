82dadb06c21c26325a0de50aac86bf35f9cfc67f
israaHamdi <israaaburayya2012@gmail.com>
2022-02-16 12:19:42 +0200

    handle error RELATES#17

--- js/logic.js
+++ js/logic.js
@@ -4,13 +4,21 @@ const fetch = (method,url,cb) =>{
         if (xhr.readyState == 4) {
             if (xhr.status == 200){
                 cb(JSON.parse(xhr.responseText))
-            }else if (xhr.status == 404) {
-                console.log('error not found')
-            }else if (xhr.status == 500) {
-                console.log("error 500")
+            }
+            if (xhr.status == 404) {
+                handleError()
             }
         }
     };
     xhr.open(method, url);
     xhr.send();
+}
+const getElement = (element) => {
+    return document.querySelector(element)
+}
+const handleError =()=> {
+    getElement('.error-sec').style.visibility="visible"; 
+    getElement('.figure').style.visibility="hidden"
+    getElement('.important-sec').style.visibility="hidden"
+    getElement('.daily-sec').style.visibility="hidden"
 }
